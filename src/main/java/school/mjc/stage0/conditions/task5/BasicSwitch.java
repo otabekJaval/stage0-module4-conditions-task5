813562a37a49bff459063d8a7326f5bae7db7bcc
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-23 06:43:23 +0500

    Update BasicSwitch.java

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -4,18 +4,10 @@ public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
-            default:
-                System.out.println("default");
-                break;
-            case 7:
-                System.out.println("seven");
-                break;
-            case 10:
-                System.out.println("ten");
-                break;
-            case 20:
-                System.out.println("twenty");
-                break;
+            default -> System.out.println("default");
+            case 7 -> System.out.println("default");
+            case 10 -> System.out.println("default");
+            case 20 -> System.out.println("default");
         }
     }
 }
